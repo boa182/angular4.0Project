5abc6114db85d40dfee3b9c34b0bac2cd4199e6d
boa <281189459@qq.con>
2018-02-28 19:51:57 +0800

    新建

---
+++ api/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+//链接数据库模块
+var mysql = require("mysql");
+
+
+//连接服务器配置.......................................................................
+function createConnection() {
+	var connection = mysql.createConnection({
+		host: '10.3.136.153',// 127.0.0.1
+		user: 'root',
+		password: '',
+		database: 'try'
+	});
+	return connection
+}
+//解决跨域
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+   	res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    next();
+});
+
+
+app.use(express.static('config'));
+// parse application/json 
+
+//get请求.................................................................................
+
+
+//要post请求...............................................................................
+// parse application/x-www-form-urlencoded 
+//使用bodyParser模块
+//用于post请求获取参数
+var bodyParser = require('body-parser')
+
+
+//监听该端口..............................................................................
+var server = app.listen(3000, function() {
+	//测试
+	//测试
+	var host = server.address().address
+	var port = server.address().port
+	console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
