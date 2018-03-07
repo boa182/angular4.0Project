bb9b142ba318f59ba871a2dc49c812682ef524fa
boa <281189459@qq.con>
2018-03-07 22:58:54 +0800

    购物车部分功能实现

--- lefeng/src/app/components/car/car.component.ts
+++ lefeng/src/app/components/car/car.component.ts
@@ -3,28 +3,73 @@ import { Component, OnInit } from '@angular/core';
 //依赖组件模块引用
 import { HttpService } from './../../utils/http.service';
 import { CommonService } from './../../utils/common.service';
-
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss']
 })
 export class CarComponent implements OnInit {
-	carType: string = null;
+	style:Object ={'display': 'none'};
+	Res:Array<any>=[];
+	carRes:Array<any>=[];
+	QtyRes:Array<any>=[];
+	qtyArr:Array<any>=[];
+	count:any = null;
   constructor(private http: HttpService,private common:CommonService) { }
 
   ngOnInit() {
-  	if(this.common.carType=='0'){
-  		//控制购物车不为空页面显示与否
-  		this.carType = this.common.carType;
-  		
-  	}
-  	
+  	this.createList();
   }
-  changcount(){
-  	console.log(123);
+  addQty(gid){
+  	
+  	this.http.post('updateqty',{
+  		uid:0,
+			gid:gid,
+  	}).then((res)=>{
+  		this.count = 0;
+  		this.createList()
+  	})
+  	
   }
 	del(){
 		console.log("del")
+	}
+	reduceQty(gid,qty){
+		if(qty>1){
+			
+			this.http.post('reduceqty',{
+	  		uid:0,
+				gid:gid,
+	  	}).then((res)=>{
+	  		this.count = 0;
+	  		this.createList()
+	  	})
+			
+		}
+	}
+	createList(){
+		
+  	this.http.get('selectqty',{
+  		uid:0
+  	}).then((qtyres)=>{
+  		let QtyRes = JSON.parse(JSON.stringify(qtyres));
+  		this.qtyArr = QtyRes;
+  		this.http.get('connetGoods',{
+  			uid:0
+  		}).then((res)=>{
+  			let Res = JSON.parse(JSON.stringify(res));
+  			this.carRes = Res;
+  			for(var i = 0;i<this.carRes.length;i++){
+  				if(this.carRes[i].gid==this.qtyArr[i].gid){
+  					this.carRes[i].qty = this.qtyArr[i].qty;
+  					this.count += this.carRes[i].qty*this.carRes[i].vipshopPrice;
+  					
+  				}
+  			}
+  		})
+  		
+  	})
+  	
 	}
 }
--- lefeng/src/app/components/pinpai/pinpai.component.ts
+++ lefeng/src/app/components/pinpai/pinpai.component.ts
@@ -1,70 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pinpai',
-  templateUrl: './pinpai.component.html',
-  styleUrls: ['./pinpai.component.scss']
+selector: 'app-pinpai',
+templateUrl: './pinpai.component.html',
+styleUrls: ['./pinpai.component.scss']
 })
 export class PinpaiComponent implements OnInit {
-
+	Adataset:Array <object>
     constructor() { }
 
 
-  ngOnInit() {
-  }
-
-
-
-
-
-
+ngOnInit() {
+}
   
 
-  Adataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'丽得姿'},
-      {img:'../../../assets/images/l8.jpg',name:'发朵'},
-      {img:'../../../assets/images/l9.jpg',name:'欧莱雅'},
-      {img:'../../../assets/images/l10.jpg',name:'纤丝鸟'},
-      {img:'../../../assets/images/l2.jpg',name:'伊丽莎白.长耳熊'}，
-      {img:'../../../assets/images/l3.jpg',name:'美洲野牛'}，
-      {img:'../../../assets/images/l4.jpg',name:'LEADERS INSOLUTION'}，
-      {img:'../../../assets/images/l5.jpg',name:'玛吉卡'}，
-  ]
-
-  Bdataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'皮尔·卡丹'},
-      {img:'../../../assets/images/l8.jpg',name:'Nuxe'},
-      {img:'../../../assets/images/l9.jpg',name:'骆驼'},
-      {img:'../../../assets/images/l10.jpg',name:'五羊'},
-      {img:'../../../assets/images/l2.jpg',name:'ELASTINE'}，
-      {img:'../../../assets/images/l3.jpg',name:'LG'}，
-  ]
-
-  Cdataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'奢步士'},
-      {img:'../../../assets/images/l8.jpg',name:'YZC'},
-      {img:'../../../assets/images/l9.jpg',name:'物生物'},
-      {img:'../../../assets/images/l10.jpg',name:'Tangle Teezer'},
-  ]
-
-  Ddataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'森田药妆'},
-      {img:'../../../assets/images/l8.jpg',name:'THE SAEM'},
-      {img:'../../../assets/images/l9.jpg',name:'皮尔·卡丹'},
-      {img:'../../../assets/images/l10.jpg',name:'婵真'},
-      {img:'../../../assets/images/l2.jpg',name:'丽得姿'}，
-  ]
-
-  Edataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'伊加'},
-      {img:'../../../assets/images/l8.jpg',name:'迪赛尼斯'},
-      {img:'../../../assets/images/l9.jpg',name:'红色小象'},
-      {img:'../../../assets/images/l10.jpg',name:'Tangle Teezer'},
-  ]
-
-  Fdataset:Array<any>=[
-      {img:'../../../assets/images/l7.jpg',name:'艾欧唯'},
-      {img:'../../../assets/images/l8.jpg',name:'福海堂'},
-      {img:'../../../assets/images/l9.jpg',name:'too cool for school'},
-  ]
+//Adataset=[
+//    {img:'./../../../assets/images/l7.jpg',name:'丽得姿'},
+//    {img:'./../../../assets/images/l8.jpg',name:'发朵'},
+//    {img:'../../../assets/images/l9.jpg',name:'欧莱雅'},
+//    {img:'../../../assets/images/l10.jpg',name:'纤丝鸟'},
+//    {img:'../../../assets/images/l2.jpg',name:'伊丽莎白.长耳熊'}，
+//    {img:'../../../assets/images/l3.jpg',name:'美洲野牛'}，
+//    {img:'../../../assets/images/l4.jpg',name:'LEADERS INSOLUTION'}，
+//    {img:'../../../assets/images/l5.jpg',name:'玛吉卡'}，
+//]
+//
+//Bdataset:Array<any>=[
+//    {img:'../../../assets/images/l7.jpg',name:'皮尔·卡丹'},
+//    {img:'../../../assets/images/l8.jpg',name:'Nuxe'},
+//    {img:'../../../assets/images/l9.jpg',name:'骆驼'},
+//    {img:'../../../assets/images/l10.jpg',name:'五羊'},
+//    {img:'../../../assets/images/l2.jpg',name:'ELASTINE'}，
+//    {img:'../../../assets/images/l3.jpg',name:'LG'}，
+//]
+//
+//Cdataset:Array<any>=[
+//    {img:'../../../assets/images/l7.jpg',name:'奢步士'},
+//    {img:'../../../assets/images/l8.jpg',name:'YZC'},
+//    {img:'../../../assets/images/l9.jpg',name:'物生物'},
+//    {img:'../../../assets/images/l10.jpg',name:'Tangle Teezer'},
+//]
+//
+//Ddataset:Array<any>=[
+//    {img:'../../../assets/images/l7.jpg',name:'森田药妆'},
+//    {img:'../../../assets/images/l8.jpg',name:'THE SAEM'},
+//    {img:'../../../assets/images/l9.jpg',name:'皮尔·卡丹'},
+//    {img:'../../../assets/images/l10.jpg',name:'婵真'},
+//    {img:'../../../assets/images/l2.jpg',name:'丽得姿'}，
+//]
+//
+//Edataset:Array<any>=[
+//    {img:'../../../assets/images/l7.jpg',name:'伊加'},
+//    {img:'../../../assets/images/l8.jpg',name:'迪赛尼斯'},
+//    {img:'../../../assets/images/l9.jpg',name:'红色小象'},
+//    {img:'../../../assets/images/l10.jpg',name:'Tangle Teezer'},
+//]
+//
+//Fdataset:Array<any>=[
+//    {img:'../../../assets/images/l7.jpg',name:'艾欧唯'},
+//    {img:'../../../assets/images/l8.jpg',name:'福海堂'},
+//    {img:'../../../assets/images/l9.jpg',name:'too cool for school'},
+//]
 }
