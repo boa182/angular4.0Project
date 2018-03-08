491b46bce6033bce473b44ee46f9a08e41813949
hehesha <634450448@qq.com>
2018-03-08 22:11:06 +0800

    图片上传后端配置

--- BM/backStage/src/app/components/addgoods/addgoods.component.ts
+++ BM/backStage/src/app/components/addgoods/addgoods.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../../utils/http.service';
 
 @Component({
   selector: 'app-addgoods',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddgoodsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpservice:HttpService) { }
 
   ngOnInit() {
   }
--- api/app.js
+++ api/app.js
@@ -2,6 +2,33 @@ var express = require('express');
 var app = express();
 //链接数据库模块
 var mysql = require("mysql");
+//form表单需要的中间件。
+var mutipart= require('connect-multiparty');
+var mutipartMiddeware = mutipart();
+
+//下面会修改临时文件的储存位置，如过没有会默认储存别的地方，这里不在详细描述。
+app.use(mutipart({uploadDir:'./images'}));
+
+//浏览器访问localhost会输出一个html文件
+app.get('/',function (req,res) {
+    res.type('text/html');
+    res.sendfile('public/index.html')
+
+});
+
+//这里就是接受form表单请求的接口路径，请求方式为post。
+app.post('/upload',mutipartMiddeware,function (req,res) {
+    //这里打印可以看到接收到文件的信息。
+    console.log(req.files);
+    /*//do something
+    * 成功接受到浏览器传来的文件。我们可以在这里写对文件的一系列操作。例如重命名，修改文件储存路径 。等等。
+    *
+    *
+    * */
+
+    //给浏览器返回一个成功提示。
+    res.send('upload success!');
+});
 
 
 //连接服务器配置.......................................................................
